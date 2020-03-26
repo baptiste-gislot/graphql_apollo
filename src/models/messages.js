61ff630e5e8313401fb93703963a51c2a2ea1b04
kobesan <baptiste.gislot@gmail.com>
2020-03-26 10:53:36 +0100

    added custom error messages

--- src/models/messages.js
+++ src/models/messages.js
@@ -2,6 +2,12 @@ const messages = (sequelize, DataTypes) => {
   const Message = sequelize.define('message', {
     text: {
       type: DataTypes.STRING,
+      validate: { 
+        notEmpty: {
+          args: true,
+          msg: 'Un message doit avoir du texte.'
+        } 
+      },
     },
   });
 
